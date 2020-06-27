8f6caaf729f17caaa5af0aac98753efaeb68e1a6
Douglas Poveda <douglas.poveda@sundevs.com>
2020-06-27 17:34:37 -0500

    Fix loading state bug

--- lib/src/features/comic/comics/comics_page.dart
+++ lib/src/features/comic/comics/comics_page.dart
@@ -12,50 +12,41 @@ class ComicsPagePageConnector extends StatelessWidget {
   Widget build(BuildContext context) {
     return StoreConnector<AppState, ComicsViewModel>(
       model: ComicsViewModel(),
-
       builder: (BuildContext context, ComicsViewModel vm) {
-          print("ComicsViewModel ${vm.isLoading}");
+        print("ComicsViewModel ${vm.isLoading}");
         return ComicsPage(
-            comics: vm.comics,
-            isLoading: vm.isLoading,
-            onGet: vm.onGetComics,
+          comics: vm.comics,
+          isLoading: vm.isLoading,
+          onGet: vm.onGetComics,
         );
-      } ,
+      },
     );
   }
 }
 
 class ComicsPage extends StatefulWidget {
-
   final List<Result> comics;
   bool isLoading;
   VoidCallback onGet;
 
-  ComicsPage({
-    this.comics,
-    this.isLoading,
-    this.onGet
-  });
+  ComicsPage({this.comics, this.isLoading, this.onGet});
 
   @override
   _ComicsPageState createState() => _ComicsPageState();
 }
 
 class _ComicsPageState extends State<ComicsPage> {
-
   bool _isGridViewStyle;
-
 
   @override
   void initState() {
     _isGridViewStyle = true;
-     widget.onGet();
+    widget.onGet();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Image(
@@ -65,7 +56,7 @@ class _ComicsPageState extends State<ComicsPage> {
         ),
         actions: <Widget>[
           IconButton(
-            icon: Icon( Icons.list ),
+            icon: Icon(Icons.list),
             onPressed: () {
               setState(() {
                 _isGridViewStyle = false;
@@ -73,12 +64,12 @@ class _ComicsPageState extends State<ComicsPage> {
             },
           ),
           IconButton(
-            icon: Icon( Icons.grid_on ),
-            onPressed: (){
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
               setState(() {
                 _isGridViewStyle = true;
               });
-            } ,
+            },
           ),
         ],
       ),
@@ -91,60 +82,53 @@ class _ComicsPageState extends State<ComicsPage> {
     );
   }
 
-  Widget _comicsList(){
-    return  widget.comics.isEmpty ?
-    Center( child: CircularProgressIndicator()) : (
-        _isGridViewStyle ? _gridComic(widget.comics):_listComic(widget.comics)
-    );
-
+  Widget _comicsList() {
+    return widget.isLoading
+        ? Center(child: CircularProgressIndicator())
+        : (_isGridViewStyle
+            ? _gridComic(widget.comics)
+            : _listComic(widget.comics));
   }
 
-  Widget _listComic(List<Result> comics){
+  Widget _listComic(List<Result> comics) {
     return ListView.builder(
-
       padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
       itemCount: comics.length,
       itemBuilder: (context, i) => _buildComic(comics[i], false),
     );
   }
 
-  Widget _gridComic(List<Result> comics){
-
+  Widget _gridComic(List<Result> comics) {
     var size = MediaQuery.of(context).size;
 
     final double itemHeight = (size.height - kToolbarHeight - 24) / 2;
     final double itemWidth = size.width / 2;
 
     return GridView.builder(
-
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 3,
-        childAspectRatio: (itemWidth / itemHeight),
-        mainAxisSpacing: 2
-      ),
-
+          childAspectRatio: (itemWidth / itemHeight),
+          mainAxisSpacing: 2),
       itemBuilder: (context, index) {
         return _buildComic(comics[index], true);
       },
     );
-
   }
 
-  Widget _buildComic(Result result,bool isRow){
-
-
+  Widget _buildComic(Result result, bool isRow) {
     return GestureDetector(
       child: Card(
         child: Container(
-          height: 300,
-          padding: EdgeInsets.all(10),
-          child: (isRow) ? Column(
-            children: buildInfo(result),
-          ) : Row(
-            children: buildInfo(result),
-          )
-        ),
+            height: 300,
+            padding: EdgeInsets.all(10),
+            child: (isRow)
+                ? Column(
+                    children: buildInfo(result),
+                  )
+                : Row(
+                    children: buildInfo(result),
+                  )),
       ),
       onTap: () {
         Navigator.push(
@@ -153,13 +137,12 @@ class _ComicsPageState extends State<ComicsPage> {
             return ComicDetailPage(result);
           }),
         );
-
       },
     );
   }
 
-  List<Widget> buildInfo(Result result){
-    return  <Widget> [
+  List<Widget> buildInfo(Result result) {
+    return <Widget>[
       Expanded(
         flex: 2,
         child: _loadImage(result),
@@ -170,31 +153,27 @@ class _ComicsPageState extends State<ComicsPage> {
     ];
   }
 
-  Widget _textDescription(Result result){
+  Widget _textDescription(Result result) {
     return ListTile(
       title: Text(
-        '${ result.name } # ${ result.issueNumber }',
+        '${result.name} # ${result.issueNumber}',
         textAlign: TextAlign.center,
       ),
-      subtitle: Text(
-          result.dateFormat(),
-          textAlign: TextAlign.center
-      ),
+      subtitle: Text(result.dateFormat(), textAlign: TextAlign.center),
       onTap: () => print(result.name),
     );
   }
 
-  Widget _loadImage(Result result){
-
-    if(result.image.screenUrl == null){
+  Widget _loadImage(Result result) {
+    if (result.image.screenUrl == null) {
       return Image(image: AssetImage('assets/no-image.png'));
-    }else{
+    } else {
       return Hero(
         tag: result.id,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
           child: FadeInImage(
-            image: NetworkImage( result.image.originalUrl),
+            image: NetworkImage(result.image.originalUrl),
             placeholder: AssetImage('assets/images/giphy.gif'),
             height: 250.0,
             width: double.infinity,
@@ -204,6 +183,4 @@ class _ComicsPageState extends State<ComicsPage> {
       );
     }
   }
-
-
 }
--- lib/src/features/comic/comics/comics_view_model.dart
+++ lib/src/features/comic/comics/comics_view_model.dart
@@ -4,24 +4,22 @@ import 'package:comicbook/src/models/comic_response.dart';
 import 'package:comicbook/src/redux/app_state.dart';
 import 'package:flutter/material.dart';
 
-class ComicsViewModel extends BaseModel<AppState>{
-
+class ComicsViewModel extends BaseModel<AppState> {
   List<Result> comics;
   bool isLoading;
   VoidCallback onGetComics;
 
   ComicsViewModel();
 
-  ComicsViewModel.build({
-    @required this.comics,
-    @required this.isLoading,
-    @required this.onGetComics
-  }) : super(equals: [comics]);
+  ComicsViewModel.build(
+      {@required this.comics,
+      @required this.isLoading,
+      @required this.onGetComics})
+      : super(equals: [comics, isLoading]);
 
   @override
   ComicsViewModel fromStore() => ComicsViewModel.build(
-    comics: state.comicState.comics,
-    isLoading: state.isLoading,
-    onGetComics: () => dispatch(ComicsAction())
-  );
+      comics: state.comicState.comics,
+      isLoading: state.isLoading,
+      onGetComics: () => dispatch(ComicsAction()));
 }
